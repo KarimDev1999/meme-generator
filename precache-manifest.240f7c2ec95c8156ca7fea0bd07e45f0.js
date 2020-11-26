c8cd21aa829fecabf2016a4afaca9750c1b32b6a
Karim <cseed322@gmail.com>
2020-11-26 13:48:51 +0300

    Updates

---
+++ precache-manifest.240f7c2ec95c8156ca7fea0bd07e45f0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48985487340a9166d6a386a7f5b9b110",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "22a97aa34b023a39a014",
+    "url": "/meme-generator/static/css/main.b8576fec.chunk.css"
+  },
+  {
+    "revision": "0c423a389801d00f91ad",
+    "url": "/meme-generator/static/js/2.50d08977.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/meme-generator/static/js/2.50d08977.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22a97aa34b023a39a014",
+    "url": "/meme-generator/static/js/main.85210047.chunk.js"
+  },
+  {
+    "revision": "163fc7ea317d65653242",
+    "url": "/meme-generator/static/js/runtime-main.66d5941a.js"
+  },
+  {
+    "revision": "24f86ea58261881f27281091070d1ef7",
+    "url": "/meme-generator/static/media/logo.24f86ea5.png"
+  },
+  {
+    "revision": "700b5e46facf671dd322e15d50bf8b8a",
+    "url": "/meme-generator/static/media/question.700b5e46.svg"
+  }
+]);
